f3a0f8f66fa4b469895a5b38d80590e057ad511c
yavishtsinhparmar-cpu <yavishtsinhparmar@gmail.com>
2026-09-25 15:20:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,10 +20,10 @@ st.set_page_config(
 GROUP_NUMBER = "Group No. 01"
 
 MEMBERS = [
-    "Member 1 - "Dev Manan Mehta - 25012250610001.",
-    "Member 2 - "Parmar Yavishtsinh Pinakin-25012250610006"
-    "Member 3 - "Devam KokilBhai Dave-25012250610005",
-    "Member 4 - "Laklhani Fenil Nileshbhai-25012250610012",
+    "Member 1 - Dev Manan Mehta - 25012250610001",
+    "Member 2 - Parmar Yavishtsinh Pinakin-25012250610006"
+    "Member 3 - Devam KokilBhai Dave-25012250610005",
+    "Member 4 - Laklhani Fenil Nileshbhai-25012250610012",
 ]
 
 # =========================================================
